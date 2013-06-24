859da78ce39fe651248fb735c20b0314496347a0
Izhar Firdaus <izhar@inigo-tech.com>
2013-06-24 18:21:08 +0800

    added translation strings for portal_actions (re #69)

---
+++ reddplusid/locales/strings.py
@@ -0,0 +1,10 @@
+from reddplusid.locales import MessageFactory as _
+
+# just some strings thats available for translation
+_(u'Projects')
+_(u'Partners')
+_(u'REDD+ Agency')
+_(u'Missions & Events')
+_(u'REDD+ In The News')
+_(u'Faceted Search')
+_(u'Help')
