7c38b92ecb5a0e1911dd37cd58e0b4c3459539f2
Izhar Firdaus <izhar@inigo-tech.com>
2013-06-24 18:28:25 +0800

    added translation strings for stats portlet and getting started portlet

--- reddplusid/locales/strings.py
+++ reddplusid/locales/strings.py
@@ -8,3 +8,15 @@ _(u'Missions & Events')
 _(u'REDD+ In The News')
 _(u'Faceted Search')
 _(u'Help')
+_(u'site_statistics', default=u'Site Statistics')
+_(u'resources', default=u'Resources')
+_(u'members', default=u'Members')
+_(u'missions', default=u'Missions')
+_(u'getting_started', default=u'Getting Started')
+_(u'getting_started_text_beforelink', default=u'''
+For a quick overview click visit your ''')
+_(u'getting_started_text_afterlink', default=u'''
+which is always accessible by clicking on your name on 
+the drop down menu on the top right of the screen.
+''')
+_(u'dashboard', default=u'dashboard')
